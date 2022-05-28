df8a4c3a9312865d37dd33c08f5f2c95d66726a7
zzLinus <2385747226@qq.com>
2022-05-28 22:07:59 +0800

    add .clangd to ignore uncompatible complie_command.json flags generate for gcc

--- .ccls-cache/@home@zz@Dev@C@kernelstuff@kernelModules/testmodules.c
+++ .ccls-cache/@home@zz@Dev@C@kernelstuff@kernelModules/testmodules.c
@@ -4,11 +4,11 @@
 MODULE_LICENSE("GPL");
 
 static int hello_init(void) {
-  printk(KERN_ALERT "Hello, world\n");
+  printk(KERN_ALERT "My kernel module!!!\n");
   return 0;
 }
 
-static void hello_exit(void) { printk(KERN_ALERT "Goodbye, cruel world\n"); }
+static void hello_exit(void) { printk(KERN_ALERT "Bye my kernel module\n"); }
 
 module_init(hello_init);
 module_exit(hello_exit);
